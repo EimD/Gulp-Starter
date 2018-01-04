afbff59468787956b4d37fd1807d2c2a9938f3bf
Eimantas <megasas@micro.lt>
2018-01-04 02:37:11 +0200

    Stayed with old version. Need to find better solution for wwwroot.

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,29 @@
 const gulp = require('gulp'),
-    clean = require('gulp-clean'),
+    clean = require('gulp-clean');
     browserSync = require('browser-sync').create(),
     sass = require('gulp-sass');
 
-
-const paths = {
-    cleanPath: ['./wwwroot/css/*', './wwwroot/html/*'],
-    sassPath: ['src/sass/*.scss']
-}
-
-// Clean wwwroot css and html folders
-// Make by folders
-// gulp.task('clean', () => {
-//     return gulp.src(paths.cleanPath, {read: false})
-//     .pipe(clean());
-//    });
-
+gulp.task('clean', function () {
+    return gulp.src("src/css")
+        .pipe(clean());
+});
 
 // Compile Sass
-gulp.task('sass', () => {
+gulp.task('sass', ['clean'],() => {
     return gulp.src(["src/sass/*.scss"])
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest("./wwwroot/css"))
+        .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
 
-// // Copy Index
-// gulp.task('copyIndex', () => {
-//     return gulp.src("./src/index.html")
-//         .pipe(gulp.dest("./wwwroot/"));
-// });
-
-// Copy HTML
-gulp.task('copyHtml', () => {
-    return gulp.src("./src/html/**")
-        .pipe(gulp.dest("./wwwroot/"));
-});
-
 // Watch & Serve
-gulp.task('serve', ['sass','copyHtml'], () => {
+gulp.task('serve', ['sass'], () => {
     browserSync.init({
-        server: './wwwroot'
+        server: './src'
     });
 
     gulp.watch(["src/sass/*.scss"], ['sass']);
-    gulp.watch("./src/html/**", ['copyHtml']);
-    gulp.watch(['./wwwroot/**/*.html']).on('change', browserSync.reload);
+    gulp.watch(['./src/**/*.html']).on('change', browserSync.reload);
 });
 
 // Default
